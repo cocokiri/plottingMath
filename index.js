fd6dc9b5909c4994524c4b9e508c5454ca43b885
Markus <strasser.ms@gmail.com>
2018-04-16 15:36:44 +0200

    x and y scaling

--- index.js
+++ index.js
@@ -8,11 +8,16 @@ const PASTELLHEX = ['#a7ff74', '#ffe8e8', '#81b4ff', '#ffa9a9', '#f7ffa6', '#c5b
 
 
 const pSize = 200;
-const plot = new SVG('plot').size(pSize, pSize);
+const plot = new SVG('plot').size(pSize * 2 , pSize * 2);
 
 const g = plot.group();
 
-g.path().attr('d', 'M 0 100 L 100 100') //sigmoidLine({from: node, to: target}))
+g.path().attr('d', 'M 0 200 L 0 0') //sigmoidLine({from: node, to: target}))
+    .attr('stroke-width', 2)
+    .attr('stroke-opacity', 0.8)
+    .attr('stroke', `black` )
+
+g.path().attr('d', 'M 0 200 L 200 200') //sigmoidLine({from: node, to: target}))
         .attr('stroke-width', 2)
         .attr('stroke-opacity', 0.8)
         .attr('stroke', `black` )
@@ -21,25 +26,50 @@ g.path().attr('d', 'M 0 100 L 100 100') //sigmoidLine({from: node, to: target}))
 
 const INCREMENT = 0.1;
 
-const f = plotF(expCos, pSize-20);
+//const f = plotFn(cos, pSize-20);
 
-for (let i =0; i < 10/INCREMENT; i++) {
-    g.path()
-        .attr('d', `M ${i} ${pSize- f(i)} L ${i+1} ${pSize - f(i+1)}`)
-        .attr('stroke-width', 0.3)
-        .attr('stroke-opacity', 0.8)
-        .attr('stroke', `black` )
+let perUnitScale = 1000;
+
+function plotter (f, startX=0, endX=20, steps=200, pixPerX = 10, yRange=2) {
+
+    const range = endX -startX;
+    const increment = range/steps;
+    const yScaler = pSize / yRange;
+    for (let x=startX; x<endX; x+=increment) {
+        const v = {
+            x: x * pixPerX,
+            y: pSize - (f(x) * yScaler),
+            x2: (x+ increment) * pixPerX,
+            y2:  pSize - (f(x+increment) * yScaler)
+        }
+
+        console.log(x, f(x), v.x, v.y)
+
+        g.path()
+            .attr('d', `M ${v.x} ${v.y} L ${v.x2} ${v.y2}`)
+            .attr('stroke-width', 0.3)
+            .attr('stroke-opacity', 0.8)
+            .attr('stroke', `black` )
+    }
+
 }
 
-function plotF (func, pSize) {
+plotter(cos);
+
+function plotFn (func, pSize) {
     return function (x) {
-        return func(x) * pSize;
+        return func(x);
     }
 }
 
-function expCos (x) {
-    return Math.cos(x)
+
+function circumference(x) {
+    return 2 * x * Math.PI;
 }
+function cos(x) {
+    return Math.cos(x);
+}
+
 
 const Plot = class {
     constructor(size = {x: 100, y:100}) {
